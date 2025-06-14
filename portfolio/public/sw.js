cda744458a61c1923937aa0cdf69f2ff03db73fd
Arman Bhattacharjee <66437768+quacky20@users.noreply.github.com>
2025-06-14 15:52:09 +0530

    updated minimal sw

--- portfolio/public/sw.js
+++ portfolio/public/sw.js
@@ -1,38 +1,13 @@
-const CACHE_NAME = 'site-static-v1'
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/favicon.svg',
-        '/manifest.json',
-        '/audio/interact.mp3'
-      ]);
-    })
-  );
+  console.log('Service Worker installed');
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  console.log('Service Worker activated');
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
+  return;
 });
