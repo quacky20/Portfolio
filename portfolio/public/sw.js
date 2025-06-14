20feb10f501328edb25a9a338ad3aabc18435bce
Arman Bhattacharjee <66437768+quacky20@users.noreply.github.com>
2025-06-14 15:29:00 +0530

    updated service worker

--- portfolio/public/sw.js
+++ portfolio/public/sw.js
@@ -1,12 +1,29 @@
+const CACHE_NAME = `site-static-${Date.now()}`;
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('site-static').then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
         '/favicon.svg',
-        '/manifest.json'
+        '/manifest.json',
+        '/audio/interact.mp3'
       ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
